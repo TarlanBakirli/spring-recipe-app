652f6ac67a22c368d89fa99b22ad90fcc5f81a5d
TarlanBakirli <terlanbekirli@gmail.com>
2019-07-24 10:25:48 +0400

    added index controller and recipe and notes entities.

---
+++ src/main/java/com/tarlanbakirli/springrecipeapp/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package com.tarlanbakirli.springrecipeapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
