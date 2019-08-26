380e5ac933d08220d12f4a8da7d4f3f8ee426f08
TarlanBakirli <terlanbekirli@gmail.com>
2019-08-26 13:49:59 +0400

    added display recipe by id functionality

--- src/main/java/com/tarlanbakirli/springrecipeapp/services/RecipeService.java
+++ src/main/java/com/tarlanbakirli/springrecipeapp/services/RecipeService.java
@@ -10,4 +10,6 @@ public interface RecipeService {
 
     Set<Recipe> getRecipes();
 
+    Recipe findById(Long id);
+
 }
--- src/test/java/com/tarlanbakirli/springrecipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/tarlanbakirli/springrecipeapp/services/RecipeServiceImplTest.java
@@ -9,9 +9,13 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
 
@@ -28,6 +32,21 @@ public class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     public void getRecipes() throws Exception {
 
 
@@ -35,7 +54,7 @@ public class RecipeServiceImplTest {
         HashSet receipesData = new HashSet();
         receipesData.add(recipe);
 
-        Mockito.when(recipeRepository.findAll()).thenReturn(receipesData);
+        when(recipeRepository.findAll()).thenReturn(receipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
